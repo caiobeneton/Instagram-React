618a158cca8c987633fb6f88acffae488fe265cc
Caio Beneton <caio.beneton@gmail.com>
2022-09-16 19:49:17 -0300

    Inicializando repositório

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+function App() {
+    return (
+        <h1>Olá mundo</h1>
+    )
+}
+
+ReactDOM.render(App(), document.querySelector('.root'));
