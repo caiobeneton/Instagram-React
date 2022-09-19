58c881d3fd237d484b89a08b176f0b82689c61bc
Caio Beneton <caio.beneton@gmail.com>
2022-09-19 03:19:49 -0300

    Feat: map das sugestões

--- src/Sugestoes.js
+++ src/Sugestoes.js
@@ -4,73 +4,55 @@ import razoes from './img/razoesparaacreditar.svg'
 import adorable from './img/adorable_animals.svg'
 import small from './img/smallcutecats.svg'
 
-export default function Sugestoes(){
-    return(
-        <div class="sugestoes">
-                <div class="titulo">
-                    Sugestões para você
-                    <div>Ver tudo</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src={bad} />
-                        <div class="texto">
-                            <div class="nome">bad.vibes.memes</div>
-                            <div class="razao">Segue você</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src={chibi} />
-                        <div class="texto">
-                            <div class="nome">chibirdart</div>
-                            <div class="razao">Segue você</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src={razoes} />
-                        <div class="texto">
-                            <div class="nome">razoesparaacreditar</div>
-                            <div class="razao">Novo no Instagram</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src={adorable} />
-                        <div class="texto">
-                            <div class="nome">adorable_animals</div>
-                            <div class="razao">Segue você</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src={small} />
-                        <div class="texto">
-                            <div class="nome">smallcutecats</div>
-                            <div class="razao">Segue você</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
+function Sugestao(props) {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={props.imagem} />
+                <div class="texto">
+                    <div class="nome">{props.nome}</div>
+                    <div class="razao">Segue você</div>
                 </div>
             </div>
+
+            <div class="seguir">Seguir</div>
+        </div>
+    )
+}
+
+export default function Sugestoes() {
+
+    const sugestoes = [
+        {
+            imagem: bad,
+            nome: 'bad.vibes.memes'
+        },
+        {
+            imagem: chibi,
+            nome: 'chibirdart'
+        },
+        {
+            imagem: razoes,
+            nome: 'razoesparaacreditar'
+        },
+        {
+            imagem: adorable,
+            nome: 'adorable_animals'
+        },
+        {
+            imagem: small,
+            nome: 'smallcutecats'
+        }
+    ]
+
+    return (
+        <div class="sugestoes">
+            <div class="titulo">
+                Sugestões para você
+                <div>Ver tudo</div>
+            </div>
+
+            {sugestoes.map((s) => <Sugestao imagem={s.imagem} nome={s.nome}></Sugestao>)}
+        </div>
     )
 }
