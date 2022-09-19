ab136371f51fab7a8d66a4beda60b0c27aeedad4
Caio Beneton <caio.beneton@gmail.com>
2022-09-19 02:53:49 -0300

    Feat: alterar usuário

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,16 +1,33 @@
+import react from 'react'
 import cat from './img/catanacomics.svg'
 
-export default function Usuario(){
-    return(
+function NovoUser(props) {
+
+    const [img, setImg] = react.useState(props.imagem)
+    const [user, setUser] = react.useState(props.user)
+    const [nome, setNome] = react.useState(props.nome)
+
+    function trocaUser() {
+        setNome(prompt('Diga seu nome'))
+        setUser(prompt('Diga seu usuário'))
+    }
+
+    return (
         <div class="usuario">
-                <img src={cat} />
-                <div class="texto">
-                    <strong>catanacomics</strong>
-                    <span>
-                        Catana
-                        <ion-icon name="pencil"></ion-icon>
-                    </span>
-                </div>
+            <img onClick={() => setImg(prompt("Diga a URL da sua imagem"))} src={img} />
+            <div class="texto">
+                <strong>{user}</strong>
+                <span>
+                    {nome}
+                    <ion-icon onClick={trocaUser} name="pencil"></ion-icon>
+                </span>
             </div>
+        </div>
+    )
+}
+
+export default function Usuario() {
+    return (
+        <NovoUser imagem={cat} user={'catanacomics'} nome={'Catana'}></NovoUser>
     )
 }
